a2a92825303da7cfbb759824a1355e8db4a6b6d4
Brunito <bru.longa06@icloud.com>
2026-06-29 09:53:44 +0200

    Delete SETUP_SUPABASE.sql

--- SETUP_SUPABASE.sql
+++
@@ -1,4 +0,0 @@
-
--- Aggiungi questo se non l'hai ancora fatto (necessario per sync Google Sheets)
--- Permette l'upsert su cantina + vino
-ALTER TABLE public.vini ADD CONSTRAINT vini_cantina_vino_unique UNIQUE (cantina, vino);
